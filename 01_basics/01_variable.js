deb1c59088e630079d389dbd84578498eb5ef97d
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-01-29 08:41:30 +0000

    Variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,28 @@
+const accountId = 14453
+let accountEmail = "aishwarya@2311";
+var accountPassword = "12345"
+accountCity = "Jaipur"; //we can write without variable 
+let accountSate; // the value is not assign the out put is undefined 
+
+//const accountId = 14453
+//accountId = 2 --- Constvalue dose not change 
+console.log("accountId");
+
+//Updating values correctly
+accountEmail = "aishwarya@gmail.com"; // let value is also change
+accountPassword = "22225";  // var value is also change
+accountCity = "Pune"; // In JS we can write without variable -------- its not good for programer 
+
+ 
+console.table({accountId, accountEmail, accountPassword, accountCity, accountSate}); // we can print value at time in table format
+
+//Note 
+//Comments 
+// 1) single line comments - //
+ //  2) Multiline comments  - /*-------------- */ 
+
+ /* 
+ 1) in js mostly use const and let variables
+ 2) in old version programmer are use var variable
+
+   
