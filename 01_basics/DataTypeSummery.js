4efc399911802c12740b2d98ab4b3a313e18a69b
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-01-30 08:52:13 +0000

    VIMP  Types operater Result

--- 01_basics/DataTypeSummery.js
+++ 01_basics/DataTypeSummery.js
@@ -40,6 +40,17 @@ const myFunction = function(){
 console.log(typeof anotherId);
 */
 
+/*VIMP  Types operater Result 
+Undefined= "undefined"
+Null= "object"
+Boolean= "boolean"
+Number= "number"
+String= "string"
+Object= (native and does not implement [[Call]])	"object"
+Object= (native or host and does implement [[Call]])	"function"
+Object= (host and does not implement [[Call]])	Implementation-defined except may not be "undefined", "boolean", "number", or "string".
+*/
+
 
 
 
