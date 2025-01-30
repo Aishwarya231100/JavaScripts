bceac0bbb267d08b7634030561d2510a9d38a74b
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-01-30 07:15:24 +0000

    Operations

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -46,3 +46,56 @@ Note -------------------------------------------------------
 0  = false
 " " = false
 "Aishwarya" = true */
+
+
+//---------------------------------Operations------------------------------------
+
+//Postive to negative value convert 
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+//Basic Mathas Operastions
+/*
+1) console.log(2+2)
+2) console.log(2-2)
+3) console.log(2*2)
+4) console.log(2**2) ---------- two to the power two
+5) console.log(2/2)
+6) console.log(2%2) 
+*/
+
+let str1 = "hello"
+let str2 = " Aishwarya" //-- Frist spaces is given beucese of separet the word 
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+"2");  // --- Output is = 12
+console.log(1+"2"); // -- Output is = 12
+
+console.log("1"+2+2); // Frist is string the all the value trit as string the output is 122
+console.log(1+2+"2"); //First add the value and then string as it is print the output is 32
+
+/*
+cnsole.log(3+4*5%4); //It not good for writen in code */
+
+console.log((3+4)*5%3); // write in this format 
+
+console.log(+true); //Output is = 1
+
+//console.log(true+); //output is Error
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+// OR we can write also this format 
+//gamecounter++
+//console.log(gameCounter);
+
+
+
+
+
+
+
